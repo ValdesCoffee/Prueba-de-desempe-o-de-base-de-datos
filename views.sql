62bcbcff8ed86b53f93aeace5a2de68d66a2a516
Auri Valdes Mendoza <valdes.2022806@ensdbexcelencia.edu.co>
2026-07-06 17:13:18 -0500

    Add view for product details and inventory value
    Created a view to retrieve product information including inventory value.

--- views.sql
+++ views.sql
@@ -10,3 +10,15 @@ SELECT
     (Quantity * UnitPrice) AS TotalPrice,
     PurchaseOrder
 FROM Movements_data;
+-- Implementar un procedimiento almacenado que permita consultar los proveedores,
+-- este podrá recibir como parámetro un id de un proveedor y traer su información, de
+-- lo contrario, si se envía un NULL retornar todos los proveedores
+CREATE VIEW vw_productos AS
+SELECT
+    product_id,
+    productname,
+    category,
+    quantity,
+    unitprice,
+    (quantity * unitprice) AS valor_inventario
+FROM products_name;
